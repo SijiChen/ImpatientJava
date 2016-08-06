9ada3d3aad42f88722c656967c23f6810eb803d0
SijiChen <chensiji19891@gmail.com>
2016-08-06 11:10:42 -0400

    2

--- src/main/java/com/impatient/ch01/NumberDemo.java
+++ src/main/java/com/impatient/ch01/NumberDemo.java
@@ -7,6 +7,8 @@ public class NumberDemo {
     public static void main(String[] arg){
         System.out.println(4000000000L);
         System.out.println(0xCAFEBABE);
-        System.out.println(0b1001);
+        System.out.println(0b1001);//fix error for binary literals not supported in project structer->Module->language level
+        System.out.println(011);//fix source release 1.8 release requires target release 1.8 by : File > Settings > Build, Execution, Deployment > Java Compiler
+
     }
 }
